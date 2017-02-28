40ea7f9288b646307b755b78360d1600daf38391
virrios <victorwiken@hotmail.com>
2017-02-28 11:59:37 +0100

    Uppdated sample.c, README, and new addition of sample2.cpp

--- sample.c
+++ sample.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main()
 {
-printf("hello world");
+for(int i=0;i<5;i++){
+printf("hello world from regular old classic c\n");
+}
 return 0;
 }
---
+++ sample2.cpp
@@ -0,0 +1,21 @@
+// endl example
+#include <iostream>     // std::cout, std::end
+using namespace std;
+int main () {
+/*
+  int a=100;
+  double b=3.14;
+
+  cout << a;
+ cout << endl;              // manipulator inserted alone
+ cout << b << endl << a*b;  // manipulator in concatenated insertion
+ endl (cout);               // endl called as a regular function
+*/
+    for(int i=0;i<5;i++){
+        cout<< "hello world from c++" <<endl;
+
+        }
+
+
+  return 0;
+}
